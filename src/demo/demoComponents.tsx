1b255e0b3b447110e3f9fcdfebb748d4303e2e16
Johannes Skuterud <johann1301s@icloud.com>
2024-02-18 15:18:13 +0100

    Remove text compoinent from demo

--- src/demo/demoComponents.tsx
+++ src/demo/demoComponents.tsx
@@ -1,11 +1,9 @@
 import {PrimaryButtonDemo} from '../buttons/primarybutton/primaryButtonDemo'
 import {PrimaryTriggerDemo} from '../triggers/primarytrigger/primaryTriggerDemo'
-import {TextDemo} from '../text/textDemo'
 import {PrimaryLinkDemo} from '../links/primarylink/primaryLinkDemo'
 
 export const allComponents = {
     'primary-bottom': PrimaryButtonDemo,
     'primary-link': PrimaryLinkDemo,
-    'primary-trigger': PrimaryTriggerDemo,
-    'text': TextDemo,
+    'primary-trigger': PrimaryTriggerDemo
 }
